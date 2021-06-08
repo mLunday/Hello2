0eed54b5bdc6dde475e4991a862ada02b9c34a4b
Mara <mara.lunday@huskers.unl.edu>
2021-06-08 09:18:56 -0500

    Added foundation

--- hello.c
+++ hello.c
@@ -3,3 +3,13 @@
  * Coded by Mara Lunday
  *  June 8, 2021
  */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main(int argc, char **argv){
+
+     printf("Mara Lunday\n");
+
+     Return 0;
+ }
